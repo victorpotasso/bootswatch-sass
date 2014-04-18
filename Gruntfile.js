01bbcf99ffb833e9b87fe62990b893c1338e0412
Victor <victorpotasso@gmail.com>
2014-04-18 16:59:19 -0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,151 @@
+module.exports = function (grunt) 
+{
+  
+    /**
+     * Configuration
+     */
+
+    grunt.initConfig({
+        
+        pkg: grunt.file.readJSON('package.json'),
+
+        banner: '/*!\n' +
+            ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+            ' * Homepage: <%= pkg.homepage %>\n' +
+            ' * Copyright 2014-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license %>\n' +
+            ' * Based on Bootswatch (http://bootswatch.com/)\n' +
+            '*/\n',
+
+        builddir: "./themes",
+
+        swatch: {
+            default:{},
+            united:{},
+            yeti:{}
+        },
+
+        clean: {
+            build: {
+                src: []
+            }
+        },
+
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: false
+            },
+            dist: {
+                src: [],
+                dest: ''
+            }
+        },
+            
+        uglify: {
+            bootstrap: {
+                files: {
+                    'src/js/bootstrap.min.js': [                        
+                        'src/js/bootstrap/transition.js',
+                        'src/js/bootstrap/alert.js',
+                        'src/js/bootstrap/button.js',
+                        'src/js/bootstrap/carousel.js',
+                        'src/js/bootstrap/collapse.js',
+                        'src/js/bootstrap/dropdown.js',
+                        'src/js/bootstrap/modal.js',
+                        'src/js/bootstrap/tooltip.js',
+                        'src/js/bootstrap/popover.js',
+                        'src/js/bootstrap/scrollspy.js',
+                        'src/js/bootstrap/tab.js',
+                        'src/js/bootstrap/affix.js'
+                    ],
+                    'src/js/jquery-2.1.0.min.js': [                        
+                        'src/js/jquery-2.1.0.js'
+                    ]
+
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                  compress: false
+                },
+                files: {}
+            }     
+        },
+    });
+
+    /**
+     * Load Tasks
+     */
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');    
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+        
+    
+    /**
+     * Register Custom Tasks
+     */
+
+    /**
+     * Build task
+     * @param theme
+     */
+    grunt.registerTask('build', 'build a regular theme', function(theme) {
+
+        var concatSrc;
+        var concatDest;
+        var sassDest;
+        var sassSrc;
+        var files = {};
+        var dist = {};
+
+        // global build.scss
+        concatSrc  = '<%=builddir%>/global/build.scss';
+        
+        // dis for build.scss
+        concatDest = '<%=builddir%>/' + theme + '/build.scss';
+
+        // sass from build.scss
+        sassSrc    = [ '<%=builddir%>/' + theme + '/' + 'build.scss' ];
+
+        // sass to bootstrap.css
+        sassDest   = '<%=builddir%>/' + theme + '/bootstrap.css';
+       
+        // concat
+        dist = {src: concatSrc, dest: concatDest};
+        grunt.config('concat.dist', dist);
+        
+        //files
+        files = {}; files[sassDest] = sassSrc;
+
+        //sass
+        grunt.config('sass.dist.files', files);
+
+        //clean
+        grunt.config('clean.build.src', [sassSrc]);
+
+        // runs tasks
+        grunt.task.run(['concat', 'sass:dist', 'clean:build']);
+    });
+    
+    /**
+     * Run build task for every theme on swatch object
+     */
+    grunt.registerMultiTask('swatch', 'build a theme', function() {
+        var theme = this.target;
+        grunt.task.run('build:' + theme);
+    });
+
+    /**
+     * Set swatch task as default
+     */
+    grunt.registerTask('default', 'build a theme', function() {
+        grunt.task.run('swatch');
+    });
+};
---
+++ README.md
@@ -0,0 +1,33 @@
+# [Bootswatch](http://bootswatch.com) forked by [Victor Potasso](http://victorpotasso.com)
+
+What is it?
+-----------
+[Bootswatch](http://bootswatch.com) is a collection of free themes for [Bootstrap](http://getbootstrap.com/). Check it out at [bootswatch.com](http://bootswatch.com).
+
+Changes
+-------
+
++ Ported Bootswatch from LESS to SASS.
+
+Author
+------
+Victor Potasso           
+
++ http://github.com/victorpotasso
++ http://victorpotasso.com
+
+Thanks
+------
++ [Mark Otto](http://github.com/markdotto) and [Jacob Thornton](http://github.com/fat) for [Bootstrap](https://github.com/twitter/bootstrap).
++ [Thomas Park](http://github.com/thomaspark) for [Bootswatch](http://bootswatch.com)
+
+
+Copyright (c) Bootswatch SASS
+-----------------------------
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
